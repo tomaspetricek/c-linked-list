0a55d12437b2494202b52da460f2ac6764e58842
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-06-22 19:29:04 +0200

    Extend docs

--- clinkedlist.h
+++ clinkedlist.h
@@ -38,7 +38,7 @@ void Node_free(struct Node** head)
     struct Node* curr = *head;
     struct Node* dealloc = NULL;
 
-    while (curr!=NULL) {
+    while (curr) {
         dealloc = curr;
         curr = curr->next;
         free(dealloc);
@@ -53,14 +53,16 @@ void Node_display(struct Node* node)
 {
     struct Node* curr = node;
 
-    while (curr->next!=NULL) {
+    while (curr->next) {
         printf("%d, ", curr->data);
         curr = curr->next;
     }
     printf("%d\n", curr->data);
 }
 
-// description: returns value of a node at given index
+// description:
+// - returns value of a node at given index
+// - value at the index is passed using val parameter of the function
 // time complexity: O(n), where n is the size of the linked list
 int Node_get(struct Node* head, int idx, int* val)
 {
@@ -69,7 +71,7 @@ int Node_get(struct Node* head, int idx, int* val)
 
     struct Node* curr = head;
 
-    for (int i = 0; curr!=NULL; i++) {
+    for (int i = 0; curr; i++) {
         if (i==idx) {
             *val = curr->data;
             return EXIT_SUCCESS;
@@ -80,7 +82,12 @@ int Node_get(struct Node* head, int idx, int* val)
     return INDEX_OUT_OF_BOUNDS;
 }
 
-// description: places a node with given value at the end of the linked list
+// description:
+// - places a node with given value at the end of the linked list
+// - returns code:
+//    - NULL_POINTER - if pointer to the head node has value NULL
+//    - EXIT_FAILURE - if the new node cannot be created
+//    - EXIT_SUCCESS - if new node was successfully appended to the linked list
 // time complexity: O(n), where n is the size of the linked list
 int Node_append(struct Node* head, int val)
 {
@@ -88,7 +95,7 @@ int Node_append(struct Node* head, int val)
 
     // get to the end
     struct Node* curr = head;
-    while (curr->next!=NULL) curr = curr->next;
+    while (curr->next) curr = curr->next;
 
     // create last node
     struct Node* last = NULL;
@@ -105,13 +112,19 @@ int Node_append(struct Node* head, int val)
     return EXIT_SUCCESS;
 }
 
-// description: places new node with the value at given index
+// description:
+// - places new node with the value at given index
+// - returns code:
+//    - INDEX_OUT_OF_BOUNDS - if index passed is lower than 0 or greater than size-1
+//    - NULL_POINTER - if pointer to the head pointer is NULL or head pointer itself is NULL
+//    - EXIT_FAILURE - if the new node cannot be created
+//    - EXIT_SUCCESS - if the value was successfully inserted
 // time complexity: 0(n), where n is the size of the linked list
 int Node_insert(struct Node** head, int idx, int val)
 {
+    if (idx<0) return INDEX_OUT_OF_BOUNDS;
     if (head==NULL) return NULL_POINTER;
     if (*head==NULL) return NULL_POINTER;
-    if (idx<0) return INDEX_OUT_OF_BOUNDS;
 
     int err;
     struct Node* curr = *head;
@@ -151,7 +164,12 @@ int Node_insert(struct Node** head, int idx, int val)
     return INDEX_OUT_OF_BOUNDS;
 }
 
-// description: removes node at the given index from the linked list
+// description:
+// - removes node at the given index from the linked list
+// - returns code:
+//    - INDEX_OUT_OF_BOUNDS - if index passed is lower than 0 or greater than size-1
+//    - NULL_POINTER - if pointer to the head pointer node is NULL or head pointer itself is NULL
+//    - EXIT_SUCCESS - if node was successfully deleted
 // time complexity: O(n), where n is the size of the linked list
 int Node_delete(struct Node** head, int idx)
 {
@@ -160,27 +178,27 @@ int Node_delete(struct Node** head, int idx)
     if (*head==NULL) return NULL_POINTER;
 
     struct Node* curr = *head;
-    struct Node* dealloc = NULL;
+    struct Node* remove = NULL;
 
     // delete head
     if (idx==0) {
-        dealloc = *head;
+        remove = *head;
         *head = curr->next;
-        dealloc->next = NULL;
+        remove->next = NULL;
 
-        Node_free(&dealloc);
+        Node_free(&remove);
         return EXIT_SUCCESS;
     }
 
-    for (int i = 0; curr!=NULL; i++) {
+    for (int i = 0; curr; i++) {
 
         // delete node
         if (i+1==idx) {
-            dealloc = curr->next;
-            curr->next = dealloc->next;
-            dealloc->next = NULL;
+            remove = curr->next;
+            curr->next = remove->next;
+            remove->next = NULL;
 
-            Node_free(&dealloc);
+            Node_free(&remove);
             return EXIT_SUCCESS;
         }
         curr = curr->next;
@@ -189,7 +207,12 @@ int Node_delete(struct Node** head, int idx)
     return INDEX_OUT_OF_BOUNDS;
 }
 
-// description: returns true if the linked list contains given value, false otherwise
+// description:
+// - stores value true in parameter contains if the linked list contains given value, false otherwise
+// - result is passed using parameter contains of the function
+// - returns code:
+//    - NULL_POINTER - if pointer to the head node has value NULL
+//    - EXIT_SUCCESS - if function ran successfully
 // time complexity: O(n), where n is the size of the linked list
 int Node_contains(struct Node* head, int val, bool* contains)
 {
@@ -198,7 +221,7 @@ int Node_contains(struct Node* head, int val, bool* contains)
     *contains = false;
     struct Node* curr = head;
 
-    while (curr!=NULL) {
+    while (curr) {
         if (curr->data==val) {
             *contains = true;
             return EXIT_SUCCESS;
